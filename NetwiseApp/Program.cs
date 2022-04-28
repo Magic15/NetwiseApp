9f8b7e6f458b3314fe3d8abb34ca8278be0fd736
MM <mmur.developer@gmail.com>
2022-04-28 10:44:23 +0200

    Add project files.

---
+++ NetwiseApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetwiseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
